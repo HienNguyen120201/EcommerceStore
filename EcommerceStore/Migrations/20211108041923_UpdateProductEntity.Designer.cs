5644524ed0002144dd3ad48e8b9d917e6713cb33
binhbuibksg0123 <74866715+binhbuibksg0123@users.noreply.github.com>
2021-11-08 11:20:20 +0700

    update database
    Add main desc attribute

--- EcommerceStore/Migrations/20211108041923_UpdateProductEntity.Designer.cs
+++ EcommerceStore/Migrations/20211108041923_UpdateProductEntity.Designer.cs
@@ -4,14 +4,16 @@ using EcommerceStore.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace EcommerceStore.Migrations
 {
     [DbContext(typeof(ECommerceDbContext))]
-    partial class ECommerceDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211108041923_UpdateProductEntity")]
+    partial class UpdateProductEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
