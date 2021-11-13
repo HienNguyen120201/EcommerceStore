2b233a5268d7f76e84133433d0359eb0496b7417
luucongdinh113 <83592031+luucongdinh113@users.noreply.github.com>
2021-11-13 19:57:51 +0700

    Update payment and something

--- EcommerceStore/Controllers/PayMentController.cs
+++ EcommerceStore/Controllers/PayMentController.cs
@@ -1,7 +1,6 @@
 ﻿using EcommerceStore.Models;
 using EcommerceStore.Services;
 using Microsoft.AspNetCore.Mvc;
-using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace EcommerceStore.Controllers
@@ -20,13 +19,31 @@ namespace EcommerceStore.Controllers
                 return RedirectToAction("Login", "Home");
             BillAndBillDetailViewModel model = await _paymentService.GetBillDetalsAsync(User);
             if (model == null)
-                return View(new BillAndBillDetailViewModel());
+                model = new BillAndBillDetailViewModel();
             await _paymentService.GetInforBillAsync(User, model);
             return View(model);
         }
         [HttpPost("/payment")]
         public async Task<IActionResult> PayMent(BillAndBillDetailViewModel bill)
         {
+            if(bill.Type=="-")
+            {
+                await _paymentService.UpdateQuantityAsync(User, bill);
+                await _paymentService.GetBillUpdateAsync(User, bill);
+                return View(bill);
+            }
+            if (bill.Type == "+")
+            {
+                await _paymentService.UpdateQuantityAsync(User, bill);
+                await _paymentService.GetBillUpdateAsync(User, bill);
+                return View(bill);
+            }
+            if(bill.Type=="delete")
+            {
+                await _paymentService.DeleteProductAsync(User, bill);
+                await _paymentService.GetBillUpdateAsync(User, bill);
+                return View(bill);
+            }    
             await _paymentService.UpdateBillAsync(bill, User);
             return View(bill);
         }
--- EcommerceStore/Models/BillDetailViewModel.cs
+++ EcommerceStore/Models/BillDetailViewModel.cs
@@ -5,6 +5,8 @@
         public string ProductName {  get; set; }
         public int ProductPrice {  get; set; }
         public int Quantity {  get; set; }
-
+        public string Url { get; set; }
+        public int ProductId { get; set; }
+        public int TotalProductPrice { get; set; }
     }
 }
